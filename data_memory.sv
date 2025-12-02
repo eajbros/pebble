09844fd63362259282593d90d01a0fa06de8cf4f
eajbros <146775668+eajbros@users.noreply.github.com>
2025-12-01 20:03:47 -0800

    Implement data memory

--- data_memory.sv
+++ data_memory.sv
@@ -1,1 +1,19 @@
+// Data Memory Module
+module data_memory(
+  input        Clk,
+               WriteEnable,
+  input  [7:0] WriteData,
+               Addr,
+  output logic [7:0] ReadData
+);
 
+  logic [7:0] Mem[256];
+
+  // Write on clock rise
+  always_ff @(posedge Clk)
+    if(WriteEnable) Mem[Addr] <= WriteData;
+
+  // Read data
+  assign ReadData = Mem[Addr];
+
+endmodule
