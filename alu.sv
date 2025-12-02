78d1f2bfb1338634645cb5cf96abd7a5e7168aa1
helenascode <hphamova@ucsd.edu>
2025-12-02 09:18:09 -0800

    added shl, shr and cmp

--- alu.sv
+++ alu.sv
@@ -1,23 +1,20 @@
 module alu(
-  input  [1:0] opcode,    // Operation code (2 bits from instruction[8:7])
-  input  [7:0] dat_a,     // First operand (from register r0) - 8 bits
-  input  [7:0] dat_b,     // Second operand (from register r1) - 8 bits
-  output [7:0] result,    // ALU result - 8 bits
-  output zero             // Zero flag - 1 bit
-);
+  input [2:0] alu_op,
+  input [7:0] r0, r1,
+  output logic[7:0] result,
+  output logic zero);
 
-  reg [7:0] temp_result;  // 8-bit result
-
-  always_comb begin
-    case (opcode)
-      2'b00: temp_result = dat_a + dat_b;  // ADD
-      2'b01: temp_result = dat_a - dat_b;  // SUB
-      2'b10: temp_result = dat_a & dat_b;  // AND
-      2'b11: temp_result = dat_a | dat_b;  // OR
-    endcase
-  end
-
-  assign result = temp_result;
+always_comb begin
+  case(alu_op)
+    3'b000: result = r0 + r1;  // add
+    3'b001: result = r0 - r1;  // sub
+    3'b010: result = r0 & r1;  // and
+    3'b011: result = r0 | r1;  // or
+    3'b100: result = r0 ^ r1;  // xor
+    3'b101: result = r0 << 1;  // shl
+    3'b110: result = r0 >> 1;  // shr
+    3'b111: result = (r0 == r1) ? 8'b1 : 8'b0; // cmp
+  endcase
+end
   assign zero = (result == 8'b0);
-
 endmodule
